1b8badd703fd6e2b214a05ab10dc4c936ad89ed5
otukutun <orehaorz@gmail.com>
2014-04-17 00:03:48 +0900

    setup.shなどを追加

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd ~
+mkdir bin swap backup tmp memo tags src .trash .tmux
+mkdir -p ~/etc/profile.d
+
+#git
+curl -S -o ~/etc/profile.d/git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+curl -S -o ~/etc/profile.d/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+
+#simlink
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/Brewfile ~/Brewfile
+
+
+cd ~/dotfiles/
+
+git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
+git clone git://github.com/erikw/tmux-powerline ~/dotfiles/.vim/bundle/tmux-powerline
+git clone git://github.com/seebi/dircolors-solarized ~/dotfiles/.vim/bundle/dircolors-solarized
+vim  +NeoBundleInstall +q
