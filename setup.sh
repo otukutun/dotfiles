8835bdddab0d035114dd58e00c309f43f6d6c7dc
otukutun <orehaorz@gmail.com>
2014-04-27 20:31:48 +0900

    modify simlink

--- setup.sh
+++ setup.sh
@@ -9,14 +9,23 @@ curl -S -o ~/dotfiles/git-prompt.sh https://raw.github.com/git/git/master/contri
 curl -S -o ~/dotfiles/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 
 
-#simlink
+#bash or zsh simlink
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.profile ~/.profile
+
+#bash'sgit config simlink
+ln -sf ~/dotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -sf ~/dotfiles/git-completion.bash ~/.git-completion.bash
+
+#vim simlink
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
+
+#git simlink
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+#for mac simlink
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 
 # Mac
@@ -31,11 +40,12 @@ if [ `uname` = "Darwin" ]; then
 fi
 cd ~/dotfiles/
 
-#oh-my-zsh
+#oh-my-zsh download
 git clone git@github.com:robbyrussell/oh-my-zsh.git ~/dotfiles/oh-my-zsh.git
 
-#oh-my-zsh
+#oh-my-zsh setting
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | ZSH=~/dotfiles/zsh sh
+
 #neobundle,tmux,color-theme
 git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
 git clone git://github.com/erikw/tmux-powerline ~/dotfiles/.vim/bundle/tmux-powerline
